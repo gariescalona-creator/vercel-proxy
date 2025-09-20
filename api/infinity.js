c9a24a37d000e06b8f589f4a499a5bfea1af545d
gariescalona-creator <gariescalona@gmail.com>
2025-09-20 12:41:53 -0500

    Update infinity.js

--- api/infinity.js
+++ api/infinity.js
@@ -13,12 +13,14 @@ export default async function handler(req, res) {
     const response = await fetch(targetUrl, {
       method: 'POST',
       headers: {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/plain',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
       },
       body: archivo
     });
 
     const data = await response.text();
+    console.log("Respuesta del servidor:", data);
     res.status(response.status).send(data);
   } catch (error) {
     res.status(500).json({ error: 'Error al conectar con el servidor destino' });
